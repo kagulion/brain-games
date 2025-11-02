6f3a7f72602b338c3dec3bc28a96a369c4a55ca2
kagulion <kagulion@vk.com>
2025-11-02 11:46:24 +0300

    add: calc logic

---
+++ src/games/calc.js
@@ -0,0 +1,14 @@
+// Калькулятор — генерирует случайное выражение из двух чисел и одного оператора, правильный ответ — результат выражения.
+
+const generateRound = () => {
+  const a = Math.floor(Math.random() * 10) + 1
+  const b = Math.floor(Math.random() * 10) + 1
+  const operators = ['+', '-', '*']
+  const operator = operators[Math.floor(Math.random() * operators.length)]
+  const question = `${a} ${operator} ${b}`
+  const correctAnswer = String(eval(question))
+
+  return [question, correctAnswer]
+}
+
+// export default () => runGame(generateRound, 'Сколько будет?')
---
+++ src/index.js
@@ -0,0 +1,1 @@
+const runGame = (generateRound, description) => {}
