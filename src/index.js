c3ed1a3a03def82a0600a68aac0d8607ce76d38d
kagulion <kagulion@vk.com>
2025-11-02 15:44:40 +0300

    test: engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-const runGame = (generateRound, description) => {}
+import readlineSync from 'readline-sync'
+
+export const runGame = (generateRound, description) => {
+  console.log('Добро пожаловать в «Игры разума»')
+  console.log(description)
+
+  const name = readlineSync.question('Как вас зовут? ')
+  console.log(`Привет, ${name}!`)
+
+  for (let i = 0; i < 3; i += 1) {
+    const [question, correctAnswer] = generateRound()
+    console.log(`Вопрос: ${question}`)
+    const userAnswer = readlineSync.question('Ваш ответ: ')
+
+    if (userAnswer === correctAnswer) {
+      console.log('Правильно!')
+    } else {
+      console.log(
+        `'${userAnswer}' неверно ;(. Правильный ответ: ${correctAnswer}`
+      )
+      console.log(`Давайте попробуем снова, ${name}!`)
+      return
+    }
+  }
+
+  console.log(`Поздравляем, ${name}!`)
+}
