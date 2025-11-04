c4644bbf24558a2058699de4fbabeb1fe2de7022
kagulion <kagulion@vk.com>
2025-11-04 12:07:02 +0300

    add: prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import { runGame } from '../src/index.js'
+import { generateRound, description } from '../src/games/prime.js'
+
+runGame(generateRound, description)
---
+++ src/games/prime.js
@@ -0,0 +1,20 @@
+import { question } from 'readline-sync'
+import { getRandomNumber } from '../utils.js'
+
+export const description =
+  'Вы запустили игру «Простое число». Правила: игроку показывается число, нужно ответить "да", если оно простое, и "нет" — если нет.'
+
+const isPrime = (num) => {
+  if (num < 2) return false
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) return false
+  }
+  return true
+}
+
+export const generateRound = () => {
+  const number = getRandomNumber(2, 100)
+  const question = String(number)
+  const correctAnswer = isPrime(number) ? 'да' : 'нет'
+  return [question, correctAnswer]
+}
